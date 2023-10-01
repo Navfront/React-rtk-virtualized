2f5ee276a5f4c819830c6b07e941dbd8e9c663d7
Andrey Nazhiganov <b3rt@narod.ru>
2023-10-01 21:38:02 +0300

    Add virtualized by react-window list of products

--- src/widgets/posts/ui/index.tsx
+++ src/widgets/posts/ui/index.tsx
@@ -1,45 +1,53 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 
 import { PostCard, useGetPostsQuery } from "@entities/post";
 import { BtnToPostDetails } from "@src/features";
+import AutoSizer from "react-virtualized-auto-sizer";
+import { VariableSizeList as List } from "react-window";
 
 const Posts: FC = () => {
-  const [itemsToSkip, setItemsToSkip] = useState(0);
+  const { data } = useGetPostsQuery("0");
 
-  const { isLoading, isError, data } = useGetPostsQuery(itemsToSkip.toString());
-  console.log(isLoading, isError, data);
+  const totalItems = data ? data.total : 0;
+  const rowSizes = new Array(totalItems).fill(true).map(() => 230);
+
+  const getItemSize = (index: number) => rowSizes[index];
+
+  const Row = ({ index, style }: any) => {
+    const currentId = index + 1;
+    return (
+      <div className="" style={style}>
+        <PostCard
+          id={currentId}
+          bottomSlot={() => <BtnToPostDetails productId={currentId} />}
+        />
+      </div>
+    );
+  };
 
   return (
     <section>
-      <h2>Список постов</h2>
-      <button
-        onClick={() => {
-          setItemsToSkip((p) => p + 5);
-        }}
-      >
-        +5
-      </button>
-      <button
-        onClick={() => {
-          setItemsToSkip((p) => p - 5);
-        }}
-      >
-        -5
-      </button>
-      <ul>
-        {data &&
-          data.products.length &&
-          data.products.map((post, index) => (
-            <PostCard
-              key={post.id}
-              id={post.id}
-              cardNumber={index + itemsToSkip}
-              description={post.description}
-              title={post.title}
-              bottomSlot={() => <BtnToPostDetails productId={post.id} />}
-            />
-          ))}
-      </ul>
+      <div style={{ width: 100, height: 1 }}></div>
+
+      {data && (
+        <AutoSizer style={{ display: "contents" }}>
+          {() => {
+            return (
+              <List
+                className="List "
+                height={700}
+                itemCount={100}
+                itemSize={getItemSize}
+                width="100%"
+                itemData={data.products}
+                style={{ margin: "0 auto" }}
+              >
+                {Row}
+              </List>
+            );
+          }}
+        </AutoSizer>
+      )}
     </section>
   );
 };
